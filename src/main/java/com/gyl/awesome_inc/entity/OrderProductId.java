7e2aab62c28989b7911e7059944b080540555b52
Gary <poppingary@gmail.com>
2022-10-28 21:00:03 -0400

    Change annotations of getter and setter to annotation of data.

--- src/main/java/com/gyl/awesome_inc/entity/OrderProductId.java
+++ src/main/java/com/gyl/awesome_inc/entity/OrderProductId.java
@@ -1,17 +1,12 @@
 package com.gyl.awesome_inc.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.Hibernate;
+import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 import java.io.Serializable;
-import java.util.Objects;
 
-@Getter
-@Setter
+@Data
 @Embeddable
 public class OrderProductId implements Serializable {
     private static final long serialVersionUID = -6437228592092430174L;
@@ -21,18 +16,4 @@ public class OrderProductId implements Serializable {
 
     @Column(name = "order_id", nullable = false, length = 30)
     private String orderId;
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(productId, orderId);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        OrderProductId entity = (OrderProductId) o;
-        return Objects.equals(this.productId, entity.productId) &&
-                Objects.equals(this.orderId, entity.orderId);
-    }
 }
--- src/main/java/com/gyl/awesome_inc/entity/ShipAddressId.java
+++ src/main/java/com/gyl/awesome_inc/entity/ShipAddressId.java
@@ -1,17 +1,12 @@
 package com.gyl.awesome_inc.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.Hibernate;
+import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 import java.io.Serializable;
-import java.util.Objects;
 
-@Getter
-@Setter
+@Data
 @Embeddable
 public class ShipAddressId implements Serializable {
     private static final long serialVersionUID = -3770428377293071143L;
@@ -21,18 +16,4 @@ public class ShipAddressId implements Serializable {
 
     @Column(name = "customer_id", nullable = false, length = 30)
     private String customerId;
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(customerId, shipAddressId);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        ShipAddressId entity = (ShipAddressId) o;
-        return Objects.equals(this.customerId, entity.customerId) &&
-                Objects.equals(this.shipAddressId, entity.shipAddressId);
-    }
 }
